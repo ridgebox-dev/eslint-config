44ca67d6f86bb2ab11fbaf3f53edbe5d49e50972
Georgiy Tarasov <georgiy@ridgebox.com>
2020-11-05 12:25:09 +0500

    Add sort lines plugin

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const fs = require('fs');
+
+const folders = fs
+  .readdirSync('src', { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
 module.exports = {
   extends: ['react-app', 'plugin:prettier/recommended', 'prettier/react'],
+  plugins: ['simple-import-sort'],
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts', '*.js', '*.jsx'],
+      rules: {
+        'simple-import-sort/sort': [
+          'warn',
+          {
+            groups: [
+              // Packages. `react` related packages come first.
+              // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+              ['^react$', '^@?\\w', '^redux$'],
+              // Absolute imports
+              [`^(${folders.join('|')})(/.*|$)`],
+              // Relative imports.
+              ['^\\.'],
+              // for scss imports.
+              ['^[^.]'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
